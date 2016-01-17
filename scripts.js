2ff35d44fd957eb6a75d28395f11ab829451f4fc
Kinarki <nickfriend81@gmail.com>
2016-01-16 20:15:08 -0800

    portfolio basic version layout

---
+++ scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    //sliding divs enter animation
+    $('#enter').on('click', function(){
+        $('.left, .right').addClass('enter');
+        $('.portfolio-name').addClass('enter-name');
+        $('.portfolio-pic').addClass('enter-pic');
+        setTimeout(function(){
+            $('.space3d').removeClass('hidden');
+        },500);
+    });
+    //toggle enter and exit buttons
+    $(".enter-button").on("click", function(){
+        $('.enter-button').toggle();
+    });
+    //sliding divs exit animation
+    $('#exit').on('click', function(){
+        $('.left, .right').removeClass('enter');
+        $('.portfolio-name').removeClass('enter-name');
+        $('.portfolio-pic').removeClass('enter-pic');
+        $('.space3d').addClass('hidden');
+    });
+});
